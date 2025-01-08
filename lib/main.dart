504ac8ad8d50f7b80c19bf64ad2bd08100d68cff
Hiro <hiromichi.sase@gmail.com>
2025-01-08 17:46:37 +0900

    リスト追加フォームを表示する

--- lib/main.dart
+++ lib/main.dart
@@ -76,12 +76,46 @@ class TodoAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            child: Text('リスト追加画面（クリックで戻る）')),
+      // *** 追加する部分 ***
+      appBar: AppBar(
+        title: Text('リスト追加'),
+      ),
+      // *** 追加する部分 ***
+      body: Container(
+        // 余白を付ける
+        padding: EdgeInsets.all(64),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // テキスト入力
+            TextField(),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // リスト追加ボタン
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                onPressed: () {},
+                child: Text('リスト追加', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Container(
+              // 横幅いっぱいに広げる
+              width: double.infinity,
+              // キャンセルボタン
+              child: TextButton(
+                // ボタンをクリックした時の処理
+                onPressed: () {
+                  // "pop"で前の画面に戻る
+                  Navigator.of(context).pop();
+                },
+                child: Text('キャンセル'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
